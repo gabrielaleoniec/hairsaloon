498d69f35958a36384200406651e4b2edc64a61b
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2022-03-24 18:32:31 +0100

    Return button added

--- src/components/features/Salon/SalonHeader.tsx
+++ src/components/features/Salon/SalonHeader.tsx
@@ -2,24 +2,45 @@ import Image from "next/image";
 import StarRating from "../../ui/StarRating/StarRating";
 import styles from "./SalonHeader.module.scss";
 import { SalonType } from "../../../interfaces/salon.d";
+import { useRouter } from "next/router";
 
 type SalonHeader = {
   name: SalonType["name"];
   image: SalonType["image"];
 };
-const SalonHeader = ({ name, image }: SalonHeader) => (
-  <div className={styles.header} style={{ backgroundImage: `url(${image})` }}>
-    <div className={styles.veil} />
-    <div className={styles.elements}>
-      <div className={styles.favourite}>
-        <Image src="/assets/icons/heart.svg" alt="" width={34} height={30} />
-      </div>
-      <div className={styles.nameRating}>
-        <h1>{name}</h1>
-        <StarRating rate={5} opinionsNo={32} />
+
+const GoBack = () => {
+  const router = useRouter();
+  const href = "/";
+  return (
+    <div
+      onClick={(e) => {
+        e.preventDefault();
+        router.push(href);
+      }}
+      className={styles.goBack}
+    >
+      <Image src="/assets/icons/arrow.svg" alt="" width={34} height={30} />
+    </div>
+  );
+};
+
+const SalonHeader = ({ name, image }: SalonHeader) => {
+  return (
+    <div className={styles.header} style={{ backgroundImage: `url(${image})` }}>
+      <div className={styles.veil} />
+      <div className={styles.elements}>
+        <GoBack />
+        <div className={styles.favourite}>
+          <Image src="/assets/icons/heart.svg" alt="" width={34} height={30} />
+        </div>
+        <div className={styles.nameRating}>
+          <h1>{name}</h1>
+          <StarRating rate={5} opinionsNo={32} />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SalonHeader;
